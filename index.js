6faa08459f9b75471603083018d8a0b06dc343d4
УкрТехноСервис <uts.vlad@gmail.com>
2021-07-20 21:51:05 +0300

    добавили файлы .env и прописали port через process.env.APP_PORT

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
-const port = 3000;
+const dotenv = require('dotenv');
+dotenv.config();  //сразу после require dotenv делаем конфиг - он все что в файле .env записывает в объект process
+const port = process.env.APP_PORT;   // это вместо того, что мы раньше хардкодили: const port = 3000;
+//теперь порт берем культурно, из наших .env  переменных окружения
+
 const app = express();
 app.use(express.json());
 app.get('/home', (request, response)=>{
